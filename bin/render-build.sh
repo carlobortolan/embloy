6bdf164ae340c3164b72549ed08d6dc2df934f30
carlobortolan <carlobortolan@gmail.com>
2023-02-24 18:58:24 +0100

    Added postgresql dependency

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-d
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+bundle exec rake db:create
 bundle exec rake db:migrate
