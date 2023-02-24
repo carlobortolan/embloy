a5e5cc2c0f0d2fb2adc21aa2b05a6f49c13f35fd
carlobortolan <carlobortolan@gmail.com>
2023-02-24 21:03:43 +0100

    Added ForeignKey migration

---
+++ db/migrate/20230110120423_foreign_keys.rb
@@ -0,0 +1,18 @@
+class ForeignKeys < ActiveRecord::Migration[7.0]
+  def change
+    #  add_foreign_key :users, :locations, column: :location_id, primary_key: :location_id
+    add_foreign_key :private_users, :users, column: :id, primary_key: :id
+    add_foreign_key :company_users, :users, column: :id, primary_key: :id
+
+    add_foreign_key :jobs, :users, column: :user_id, primary_key: :id
+    # add_foreign_key :jobs, :locations, column: :location_id, primary_key: :location_id
+
+    add_foreign_key :applications, :users, column: :applicant_id, primary_key: :id
+    add_foreign_key :applications, :jobs, column: :job_id, primary_key: :job_id
+
+    add_foreign_key :notifications, :users, column: :employer_id, primary_key: :id
+    add_foreign_key :notifications, :jobs, column: :job_id, primary_key: :job_id
+
+    add_foreign_key :reviews, :users, column: :created_by, primary_key: :id
+  end
+end
