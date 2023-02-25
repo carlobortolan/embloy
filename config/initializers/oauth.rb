484e6c6a670856d71b240dc783185266f4c37fe1
carlobortolan <carlobortolan@gmail.com>
2023-02-25 23:47:16 +0100

    oauth2

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :google_oauth2, '280969066348-aovpgj7fj2ei1miu7s4gbvhqveb0jans.apps.googleusercontent.com', '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'
-  provider :google_oauth2, '280969066348-g036daovajogpcu444itd3een858q5nb.apps.googleusercontent.com', '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'
+  provider :google_oauth2, '280969066348-aovpgj7fj2ei1miu7s4gbvhqveb0jans.apps.googleusercontent.com', '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'
+  # provider :google_oauth2, '280969066348-g036daovajogpcu444itd3een858q5nb.apps.googleusercontent.com', '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'
   # provider :google_oauth2, ENV["GOOGLE_OAUTH2_KEY"], ENV["GOOGLE_OAUTH2_SECRET"]
 end
