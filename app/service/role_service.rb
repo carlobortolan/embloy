965b95fd720ff0a1453315169208e960e46088c6
github4touchdouble <touchedouble@gmx.de>
2023-03-01 13:38:10 +0100

    removed redundancy

--- app/service/role_service.rb
+++ app/service/role_service.rb
@@ -2,35 +2,72 @@
 
 class RoleService < ApplicationController
 
-  def require_admin!
+  #########################################################
+  ################# WITH DATABASE LOOKUP ##################
+  #########################################################
+  def require_admin!(id = nil) #mehtod can be called for a specific id or using Curren.user from Application Controller
+    set_current_id(id)
     return admin?(Current.user.user_role)
   end
 
-  def require_editor!
+  def require_editor!(id = nil)
+    set_current_id(id)
     return editor?(Current.user.user_role)
   end
 
-  def require_developer!
+  def require_developer!(id = nil)
+    set_current_id(id)
     return developer?(Current.user.user_role)
   end
 
-  def require_moderator!
+  def require_moderator!(id = nil)
+    set_current_id(id)
     return moderator?(Current.user.user_role)
   end
 
-  def require_verified!
+  def require_verified!(id = nil)
+    set_current_id(id)
     return verified?(Current.user.user_role)
   end
 
-  protected
-  def set_current_user(id)
-    Current.user = User.find_by(id: id)
-    if Current.user.nil?
-      raise RoleService::InvalidUser::Unknown
-    end
+  #########################################################
+  ################# NO DATABASE LOOKUP ####################
+  ########## USED FOR LOOKUP FREE TOKEN USAGE #############
+  #########################################################
+  def require_admin(user_role)
+    return super.admin?(user_role)
   end
 
+  def require_editor(user_role)
+    return super.editor?(user_role)
+  end
 
+  def require_developer(user_role)
+    return super.developer?(user_role)
+  end
+
+  def require_moderator(user_role)
+    return super.moderator?(user_role)
+  end
+
+  def require_verified(user_role)
+    return super.verified?(user_role)
+  end
+
+  protected
+
+  def set_current_id(id = nil)
+    if id.nil?
+      if Current.user.nil?
+        raise RoleService::InvalidUser::LoggedOut
+      end
+    else
+      Current.user = User.find_by(id: id)
+      if Current.user.nil?
+        raise RoleService::InvalidUser::Unknown
+      end
+    end
+  end
 
   def admin?(user_role)
     if user_role == "admin"
@@ -72,110 +109,12 @@ class RoleService < ApplicationController
     end
   end
 
-
-  class API
-
-    #########################################################
-    ################# NO DATABASE LOOKUP ####################
-    ########## USED FOR LOOKUP FREE TOKEN USAGE #############
-    #########################################################
-    def require_admin(user_role)
-      return super.admin?(user_role)
-    end
-
-    def require_editor(user_role)
-      return super.editor?(user_role)
-    end
-
-
-    def require_developer(user_role)
-      return super.developer?(user_role)
-    end
-
-    def require_moderator(user_role)
-      return super.moderator?(user_role)
-    end
-
-    def require_verified(user_role)
-      return super.verified?(user_role)
-    end
-
-    #########################################################
-    ################## DATABASE LOOKUP ######################
-    #########################################################
-    def require_admin!(id)
-      set_current_user(id)
-      return super.require_admin!
-    end
-
-    def require_editor!(id)
-      set_current_user(id)
-      return super.require_editor!
-    end
-
-
-    def require_developer!(id)
-      set_current_user(id)
-      return super.require_developer!
-    end
-
-    def require_moderator!(id)
-      set_current_user(id)
-      return super.require_moderator!
-    end
-
-    def require_verified!(id)
-      set_current_user(id)
-      return super.require_verified!
-    end
-
-  end
-
   class InvalidUser < StandardError
     class Unknown < StandardError
     end
+
     class LoggedOut < StandardError
     end
   end
 end
-=begin
-def admin?
-  if Current.user.user_role == "admin"
-    true
-  else
-    false
-  end
-end
 
-def editor?
-  if Current.user.user_role == "admin" || Current.user.user_role == "editor"
-    true
-  else
-    false
-  end
-end
-
-def developer?
-  if Current.user.user_role == "admin" || Current.user.user_role == "developer"
-    true
-  else
-    false
-  end
-end
-
-def moderator?
-  if Current.user.user_role == "admin" || Current.user.user_role == "editor" || Current.user.user_role == "moderator"
-    true
-  else
-    false
-  end
-end
-
-def verified?
-  if Current.user.user_role == "admin" || Current.user.user_role == "editor" || Current.user.user_role == "moderator" || Current.user.user_role == "verified"
-    true
-  else
-    false
-  end
-end
-=end
