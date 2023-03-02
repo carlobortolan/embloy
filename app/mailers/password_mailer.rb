2135e1f1bad29757ff2a4c4aebc41f943f939d51
carlobortolan <carlobortolan@gmail.com>
2023-03-02 14:46:44 +0100

    Updated views

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -1,6 +1,6 @@
 class PasswordMailer < ApplicationMailer
   def reset
     @token = params[:user].signed_id(purpose: 'password_reset', expires_in: 15.minutes)
-    mail from: "noreply@embloy.com", to: params[:user].email
+    mail from: ENV['EMAIL_NOREPLY_USER'], to: params[:user].email
   end
 end
