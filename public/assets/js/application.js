6f3f461d129d5265bb88cfb1111f8a935b47dcde
Carlo Bortolan <carlo.bortolan@tum.de>
2023-03-02 04:12:57 +0100

    Updated import for container

---
+++ public/assets/js/application.js
@@ -0,0 +1,16 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// import "@hotwired/turbo-rails"
+import "controllers"
+import "trix"
+import "@rails/actiontext"
+import "./main"
+
+// import { Application } from "@hotwired/stimulus"
+//
+// const application = Application.start()
+//
+// // Configure Stimulus development experience
+// application.debug = false
+// window.Stimulus   = application
+//
+// export { application }
