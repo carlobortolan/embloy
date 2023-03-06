058cf03551e67b729a8c81421d4f02c00b7808f0
github4touchdouble <touchedouble@gmx.de>
2023-03-06 23:37:16 +0100

    bugfixes

--- app/controllers/api/v0/reviews_controller.rb
+++ app/controllers/api/v0/reviews_controller.rb
@@ -59,131 +59,30 @@ module Api
       end
 
       def destroy
-        if request.headers["HTTP_ACCESS_TOKEN"].nil?
-          render status: 400, json: { "access_token": [
-            {
-              "error": "ERR_BLANK",
-              "description": "Attribute can't be blank"
-            }
-          ]
-          }
-        else
+
           begin
-            decoded_token = AuthenticationTokenService::Access::Decoder.call(request.headers["HTTP_ACCESS_TOKEN"])[0]
-            verified!(decoded_token["typ"])
+            verified!(@decoded_token["typ"])
             review = Review.find(params[:id])
 
             # Todo: Replace with general must_be_owner! method (if it then exist)
             ###############################################################################################################################
-            review.created_by.to_i == User.find(decoded_token["sub"].to_i).id ? true : raise(CustomExceptions::Unauthorized::InsufficientRole::NotOwner) #
+            review.created_by.to_i == User.find(@decoded_token["sub"].to_i).id ? true : raise(CustomExceptions::Unauthorized::InsufficientRole::NotOwner) #
             ###############################################################################################################################
 
             review.destroy!
             render status: 200, json: { "message": "Review deleted!" }
 
+          rescue ActionController::ParameterMissing
+            blank_error('review')
           rescue ActiveRecord::RecordNotFound
             if params[:id].nil?
-              render status: 400, json: { "review": [
-                {
-                  "error": "ERR_BLANK",
-                  "description": "Attribute can't be blank."
-                }
-              ]
-              }
+              blank_error('review')
             else
-              render status: 400, json: { "review": [
-                {
-                  "error": "ERR_INVALID",
-                  "description": "Attribute is malformed or unknown."
-                }
-              ]
-              }
+              malformed_error('review')
             end
           rescue ActiveRecord::StatementInvalid
-            render status: 400, json: { "review": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
+            malformed_error('review')
 
-          rescue CustomExceptions::InvalidUser::Unknown
-            render status: 400, json: { "user": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-          rescue CustomExceptions::Unauthorized::InsufficientRole
-            render status: 403, json: { "user": [
-              {
-                "error": "ERR_INACTIVE",
-                "description": "Attribute is blocked."
-              }
-            ]
-            }
-=begin
-          rescue CustomExceptions::Unauthorized::InsufficientRole::NotOwner # thrown from ApplicationController::Job.must_be_owner!
-            render status: 403, json: { "job": [
-              {
-                "error": "ERR_INACTIVE",
-                "description": "Attribute is blocked."
-              }
-            ]
-            }
-=end
-          rescue CustomExceptions::InvalidInput::Token
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-
-          rescue JWT::ExpiredSignature
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute has expired."
-              }
-            ]
-            }
-          rescue JWT::InvalidIssuerError
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute was signed by an unknown issuer."
-              }
-            ]
-            }
-          rescue JWT::VerificationError
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Token can't be verified."
-              }
-            ]
-            }
-          rescue JWT::IncorrectAlgorithm
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Token was encoded with an unknown algorithm."
-              }
-            ]
-            }
-          rescue JWT::DecodeError
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-          end
         end
       end
 
