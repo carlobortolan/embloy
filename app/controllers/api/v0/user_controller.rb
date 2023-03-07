14ac8a51b996f9995ea1fe1134a0ae6840617c54
github4touchdouble <touchedouble@gmx.de>
2023-03-07 17:44:31 +0100

    added efficiency

--- app/controllers/api/v0/user_controller.rb
+++ app/controllers/api/v0/user_controller.rb
@@ -2,244 +2,29 @@
 module Api
   module V0
     class UserController < ApiController
+      before_action :verify_access_token
 
       def own_jobs
-        if request.headers["HTTP_ACCESS_TOKEN"].nil?
-          render status: 400, json: { "access_token": [
-            {
-              "error": "ERR_BLANK",
-              "description": "Attribute can't be blank"
-            }
-          ]
-          }
-        else
-          begin
-            decoded_token = AuthenticationTokenService::Access::Decoder.call(request.headers["HTTP_ACCESS_TOKEN"])[0]
-            verified!(decoded_token["typ"])
-            jobs = User.find_by(id: decoded_token["sub"].to_i).jobs.order(created_at: :desc)
-            if jobs.empty?
-              render status: 204, json: { "jobs": jobs }
-            else
-              render status: 200, json: { "jobs": jobs }
-            end
-
-          rescue CustomExceptions::Unauthorized::InsufficientRole
-            render status: 403, json: { "user": [
-              {
-                "error": "ERR_INACTIVE",
-                "description": "Attribute is blocked."
-              }
-            ]
-            }
-
-          rescue CustomExceptions::InvalidInput::Token
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-          rescue JWT::ExpiredSignature
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute has expired."
-              }
-            ]
-            }
-          rescue JWT::InvalidIssuerError
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute was signed by an unknown issuer."
-              }
-            ]
-            }
-          rescue JWT::VerificationError
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Token can't be verified."
-              }
-            ]
-            }
-          rescue JWT::IncorrectAlgorithm
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Token was encoded with an unknown algorithm."
-              }
-            ]
-            }
-          rescue JWT::DecodeError
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-          end
+        begin
+          verified!(@decoded_token["typ"])
+          jobs = User.find(id: @decoded_token["sub"].to_i).jobs.order(created_at: :desc)
+          jobs.empty? ? render( status: 204, json: { "jobs": jobs }) : render(status: 200, json: { "jobs": jobs })
         end
       end
 
       def own_applications
-          if request.headers["HTTP_ACCESS_TOKEN"].nil?
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_BLANK",
-                "description": "Attribute can't be blank"
-              }
-            ]
-            }
-          else
-            begin
-            decoded_token = AuthenticationTokenService::Access::Decoder.call(request.headers["HTTP_ACCESS_TOKEN"])[0]
-            verified!(decoded_token["typ"])
-            applications = Application.all.where(user_id: decoded_token["sub"].to_i)
-            if applications.empty?
-              render status: 204, json: { "applications": applications }
-            else
-              render status: 200, json: { "applications": applications }
-            end
-
-            rescue CustomExceptions::Unauthorized::InsufficientRole
-              render status: 403, json: { "user": [
-                {
-                  "error": "ERR_INACTIVE",
-                  "description": "Attribute is blocked."
-                }
-              ]
-              }
-            rescue CustomExceptions::InvalidInput::Token
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-            rescue JWT::ExpiredSignature
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute has expired."
-              }
-            ]
-            }
-            rescue JWT::InvalidIssuerError
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute was signed by an unknown issuer."
-              }
-            ]
-            }
-            rescue JWT::VerificationError
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Token can't be verified."
-              }
-            ]
-            }
-            rescue JWT::IncorrectAlgorithm
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Token was encoded with an unknown algorithm."
-              }
-            ]
-            }
-            rescue JWT::DecodeError
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-
-          end
+        begin
+          verified!(@decoded_token["typ"])
+          applications = Application.all.where(user_id: @decoded_token["sub"].to_i)
+          applications.empty? ? render(status: 204, json: { "applications": applications }) : render(status: 200, json: { "applications": applications })
         end
       end
 
       def own_reviews
-        if request.headers["HTTP_ACCESS_TOKEN"].nil?
-          render status: 400, json: { "access_token": [
-            {
-              "error": "ERR_BLANK",
-              "description": "Attribute can't be blank"
-            }
-          ]
-          }
-        else
-          begin
-            decoded_token = AuthenticationTokenService::Access::Decoder.call(request.headers["HTTP_ACCESS_TOKEN"])[0]
-            verified!(decoded_token["typ"])
-            reviews = Review.all.where(subject: decoded_token["sub"].to_i)
-            reviews.empty? ? render(status: 204, json: { "reviews": reviews }) : render(status: 200, json: { "reviews": reviews })
-
-
-          rescue CustomExceptions::Unauthorized::InsufficientRole
-            render status: 403, json: { "user": [
-              {
-                "error": "ERR_INACTIVE",
-                "description": "Attribute is blocked."
-              }
-            ]
-            }
-          rescue CustomExceptions::InvalidInput::Token
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-          rescue JWT::ExpiredSignature
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute has expired."
-              }
-            ]
-            }
-          rescue JWT::InvalidIssuerError
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute was signed by an unknown issuer."
-              }
-            ]
-            }
-          rescue JWT::VerificationError
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Token can't be verified."
-              }
-            ]
-            }
-          rescue JWT::IncorrectAlgorithm
-            render status: 401, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Token was encoded with an unknown algorithm."
-              }
-            ]
-            }
-          rescue JWT::DecodeError
-            render status: 400, json: { "access_token": [
-              {
-                "error": "ERR_INVALID",
-                "description": "Attribute is malformed or unknown."
-              }
-            ]
-            }
-
-          end
+        begin
+          verified!(@decoded_token["typ"])
+          reviews = Review.all.where(subject: @decoded_token["sub"].to_i)
+          reviews.empty? ? render(status: 204, json: { "reviews": reviews }) : render(status: 200, json: { "reviews": reviews })
         end
       end
     end
