6197ac1a4d17635395179e698b3953bcc598734a
github4touchdouble <touchedouble@gmx.de>
2023-03-07 17:36:40 +0100

    added efficiency

--- app/controllers/api/v0/passwords_controller.rb
+++ app/controllers/api/v0/passwords_controller.rb
@@ -2,6 +2,7 @@
 module Api
   module V0
     class PasswordsController < ApiController
+      before_action :verify_access_token
 
       def update
         begin
@@ -35,100 +36,20 @@ module Api
             ]
             }
           else
-
-            if request.headers["HTTP_ACCESS_TOKEN"].nil?
-              render status: 400, json: { "access_token": [
-                {
-                  "error": "ERR_BLANK",
-                  "description": "Attribute can't be blank"
-                }
-              ]
-              }
-            else
-
-              decoded_token = AuthenticationTokenService::Access::Decoder.call(request.headers["HTTP_ACCESS_TOKEN"])[0]
-              verified!(decoded_token["typ"])
-              if User.find_by(id: decoded_token["sub"]).update(password_params)
-                render status: 200, json: { "message": "Password successfully updated!" }
-              else
-                render status: 422, json: { "password": [
-                  {
-                    "error": "ERR_INVALID",
-                    "description": "Attribute couldn't get updated"
-                  }
-                ]
-                }
-              end
-
-            end
+            verified!(@decoded_token["typ"])
+            User.find(id: @decoded_token["sub"]).update!(password_params)
+            render status: 200, json: { "message": "Password successfully updated!" }
           end
 
         rescue ActionController::ParameterMissing
-          render status: 400, json: { "user": [
-            {
-              "error": "ERR_BLANK",
-              "description": "Attribute can't be blank"
-            }
-          ]
-          }
+          blank_error('user')
 
-        rescue CustomExceptions::Unauthorized::InsufficientRole
-          render status: 403, json: { "user": [
-            {
-              "error": "ERR_INACTIVE",
-              "description": "Attribute is blocked."
-            }
-          ]
-          }
+        rescue ActiveRecord::RecordNotFound
+            malformed_error('user')
 
-        rescue CustomExceptions::InvalidInput::Token
-          render status: 400, json: { "access_token": [
-            {
-              "error": "ERR_INVALID",
-              "description": "Attribute is malformed or unknown."
-            }
-          ]
-          }
-        rescue JWT::ExpiredSignature
-          render status: 401, json: { "access_token": [
-            {
-              "error": "ERR_INVALID",
-              "description": "Attribute has expired."
-            }
-          ]
-          }
-        rescue JWT::InvalidIssuerError
-          render status: 401, json: { "access_token": [
-            {
-              "error": "ERR_INVALID",
-              "description": "Attribute was signed by an unknown issuer."
-            }
-          ]
-          }
-        rescue JWT::VerificationError
-          render status: 401, json: { "access_token": [
-            {
-              "error": "ERR_INVALID",
-              "description": "Token can't be verified."
-            }
-          ]
-          }
-        rescue JWT::IncorrectAlgorithm
-          render status: 401, json: { "access_token": [
-            {
-              "error": "ERR_INVALID",
-              "description": "Token was encoded with an unknown algorithm."
-            }
-          ]
-          }
-        rescue JWT::DecodeError
-          render status: 400, json: { "access_token": [
-            {
-              "error": "ERR_INVALID",
-              "description": "Attribute is malformed or unknown."
-            }
-          ]
-          }
+        rescue ActiveRecord::RecordInvalid
+          malformed_error('user')
+
         end
       end
 
