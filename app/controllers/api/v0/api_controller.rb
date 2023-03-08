9429600c21a2b4ffe4ecbb8f2847c2d6ed55a3ad
Carlo Bortolan <carlo.bortolan@tum.de>
2023-03-08 03:00:26 +0100

    Removed api_exception_handler.rb for remote deployment (to be reverted)

--- app/controllers/api/v0/api_controller.rb
+++ app/controllers/api/v0/api_controller.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
-require_relative 'api_exception_handler.rb'
 module Api
   module V0
-    class ApiController < ApplicationController
-      include ApiExceptionHandler
-      protect_from_forgery with: :null_session
-
-      def verify_access_token
-        request.headers["HTTP_ACCESS_TOKEN"].nil? ? blank_error('token') : @decoded_token = AuthenticationTokenService::Access::Decoder.call(request.headers["HTTP_ACCESS_TOKEN"])[0]
-      end
-
-    end
   end
 end
