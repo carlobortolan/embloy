93ca6eb20e24b9b69d7be6b8641afb748e94a9d0
github4touchdouble <touchedouble@gmx.de>
2023-03-10 22:06:12 +0100

    updated api tests

--- spec/requests/api/v0/registration_controllers_spec.rb
+++ spec/requests/api/v0/registration_controllers_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::V0::RegistrationControllers", type: :request do
   describe "POST /api/v0/user" do
     context 'valid normal inputs' do
+      let(:user_params) { FactoryBot.attributes_for(:user) }
       it "returns a 200" do
 
       end
