67bd16fffb8b25969a0412b814162a70b4413453
Carlo Bortolan <carlo.bortolan@tum.de>
2023-03-17 18:53:35 +0100

    Cleaned up

--- app/service/job_slicer.rb
+++ app/service/job_slicer.rb
@@ -2,13 +2,17 @@ require_relative '../../config/environment'
 
 class JobSlicer
   # Fetches a slice (=partition) containing all relevant jobs for the current user
+  # TODO: Implement using postgis
   def self.slice(user)
     puts "STARTED SLICING"
 
-    # TODO: Implement using postgis
-    jobs = Job.where("SELECT * FROM jobs LIMIT 5")
+    # Query method 1
+    # jobs = Job.where("SELECT * FROM jobs LIMIT 5")
+
+    # Query method 2
+    # query = "SELECT * FROM jobs LIMIT 5"
+    # results = ActiveRecord::Base.connection.execute(query)
 
     puts "ENDED SLICING"
   end
-
 end
