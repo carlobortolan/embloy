55d432ecfe044497ec2e848990ffc7fd4a90c7c8
github4touchdouble <touchedouble@gmx.de>
2023-03-19 23:04:04 +0100

    updated verification error messages

--- app/models/job.rb
+++ app/models/job.rb
@@ -13,12 +13,14 @@ class Job < ApplicationRecord
   validates :start_slot, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
   validates :longitude, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
   validates :latitude, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
-  validates :job_notifications, presence: true
-  validates :position, presence: true
-  validates :key_skills, presence: true
-  validates :duration, presence: true
-  validates :salary, presence: true
-  validates :currency, presence: true
+  validates :job_notifications, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
+  validates :position, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
+  validates :key_skills, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
+  validates :duration, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
+  validates :salary, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
+  validates :currency, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
+  #TODO: @cb make front end job_type submit work -> Then activate verification blow
+  #validates :job_type, presence: { "error": "ERR_BLANK", "description": "Attribute can't be blank" }
 
   def profile
     @job.update(view_count: @job.view_count + 1)
@@ -28,7 +30,7 @@ class Job < ApplicationRecord
     self.applications.each { |application| application.reject("REJECTED") }
   end
 
-  def format_address
+  def format_addeess
     "#{self.address}, #{self.city}, #{self.postal_code}, #{self.country_code}"
     if self.address.nil? || self.city.nil? || self.postal_code.nil? || self.country_code.nil?
       "No location details available."
