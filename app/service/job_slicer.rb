b6b5845d5ba1ece01bd77d91c43d549aef788a6d
github4touchdouble <touchedouble@gmx.de>
2023-03-23 09:23:28 +0100

    added geo_query

--- app/service/job_slicer.rb
+++ app/service/job_slicer.rb
@@ -5,7 +5,7 @@ class JobSlicer
   # TODO: Implement using postgis
   def self.slice(user)
     puts "STARTED SLICING"
-
+    #jobs = SpatialJobValue::geo_query_jobs(user.latitude, user.longitude, 10000) #todo: add functionality that dynamically adapts rad according to the density of jobs in the area (to better consider differences between very densely populated urban areas and rural areas)
     # Query method 1
     # jobs = Job.where("SELECT * FROM jobs LIMIT 5")
 
