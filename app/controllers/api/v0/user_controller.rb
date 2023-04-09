93e9e9fee77cb578f9fb729c5b18997c5f599d65
github4touchdouble <touchedouble@gmx.de>
2023-04-09 17:38:31 +0200

    bugfix

--- app/controllers/api/v0/user_controller.rb
+++ app/controllers/api/v0/user_controller.rb
@@ -17,16 +17,22 @@ module Api
       def own_applications
         begin
           verified!(@decoded_token["typ"])
-          applications = Application.all.where(user_id: @decoded_token["sub"].to_i)
+          user = User.find(@decoded_token["sub"].to_i)
+          applications = Application.all.where(user_id: user.id)
           applications.empty? ? render(status: 204, json: { "applications": applications }) : render(status: 200, json: { "applications": applications })
+        rescue ActiveRecord::RecordNotFound
+          not_found_error('user')
         end
       end
 
       def own_reviews
         begin
           verified!(@decoded_token["typ"])
-          reviews = Review.all.where(subject: @decoded_token["sub"].to_i)
+          user = User.find(@decoded_token["sub"].to_i)
+          reviews = Review.all.where(subject: user.id)
           reviews.empty? ? render(status: 204, json: { "reviews": reviews }) : render(status: 200, json: { "reviews": reviews })
+        rescue ActiveRecord::RecordNotFound
+          not_found_error('user')
         end
       end
     end
