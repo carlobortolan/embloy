eac6d7533679f0c88275b3804397aabdb4182af0
github4touchdouble <touchedouble@gmx.de>
2023-04-24 16:38:49 +0200

    bugfix

--- app/controllers/api/v0/reviews_controller.rb
+++ app/controllers/api/v0/reviews_controller.rb
@@ -4,19 +4,17 @@ module Api
     class ReviewsController < ApiController
       before_action :verify_access_token
       before_action :verify_path_user_id, only: [:create]
-=begin
       def create
         # todo:test, adapt and complete the api documentation for reviews accodingly, make review update, review delete method, adapt doc
 
         begin
           must_be_verified!(@decoded_token["sub"])
-
           # verify strong param input
-          if (review_params[:rating].nil? || review_params[:rating].empty?) && review_params[:job_id].present?
-            blank_error('rating')
-          elsif (review_params[:job_id].nil? || review_params[:job_id].empty?) && review_params[:rating].present?
-            blank_error('job_id')
-          elsif (review_params[:job_id].nil? || review_params[:job_id].empty?) && (review_params[:rating].nil? || review_params[:rating].empty?)
+          if (review_params[:rating].nil? || review_params[:rating].blank?) && review_params[:job_id].present?
+            return blank_error('rating')
+          elsif (review_params[:job_id].nil? || review_params[:job_id].blank?) && review_params[:rating].present?
+            return blank_error('job_id')
+          elsif (review_params[:job_id].nil? ||review_params[:job_id].blank?) && (review_params[:rating].nil? ||review_params[:rating].blank?)
             render status: 400, json:{"rating" => [{error: 'ERR_BLANK', description: 'Attribute can\'t be blank'}], "job_id" => [{error: 'ERR_BLANK', description: 'Attribute can\'t be blank'}]}
             return -1
           end
@@ -40,7 +38,7 @@ module Api
           # verify rating claim
           begin # this construction is necessary to account that a string parsed to an integer is 0 and we want to allow 0 as input
             review_params[:rating] = Integer(review_params[:rating])
-            raise ArgumentError if review_params[:rating] != 0 || review_params[:rating] != 1 || review_params[:rating] != 2 || review_params[:rating] != 3 || review_params[:rating] != 4 || review_params[:rating] != 5
+            raise ArgumentError unless review_params[:rating] == 0 || review_params[:rating] == 1 || review_params[:rating] == 2 || review_params[:rating] == 3 || review_params[:rating] == 4 || review_params[:rating] == 5
           rescue ArgumentError
             return malformed_error('rating')
           end
@@ -51,10 +49,10 @@ module Api
           return biased_error('user') if @decoded_token["sub"] == params[:id]
           if @decoded_token["sub"] == job.user_id # employer rates employee
             application = job.applications.find_by_sql("SELECT * FROM applications a WHERE a.job_id = #{job.job_id} AND a.user_id = #{params[:id]}")
-            return access_denied_error('user') if application.result.to_i != 1 #only reviews between employers and emplyees are allowed #todo: add way to know who is an actual employee
+            return access_denied_error('user') if application.nil? || application.empty? || application[0].status.to_i != 1 #only reviews between employers and emplyees are allowed #todo: add way to know who is an actual employee
           elsif params[:id] == job.user_id # employee rates employer
             application = job.applications.find_by_sql("SELECT * FROM applications a WHERE a.job_id = #{job.job_id} AND a.user_id = #{@decoded_token["sub"]}")
-            return access_denied_error('user') if application.result.to_i != 1 #only reviews between employers and emplyees are allowed #todo: add way to know who is an actual employee
+            return access_denied_error('user') if application.nil? || application.empty? || application[0].status.to_i != 1 #only reviews between employers and emplyees are allowed #todo: add way to know who is an actual employee
           else # only an employee can rate its employer based on a given job and vice versa
             return access_denied_error('user')
           end
@@ -62,16 +60,17 @@ module Api
           #--------------------------------------
 
           #verify whether this specific review already exists
-          return unnecessary_error('review') if Review.all.where(:created_by => @decoded_token["sub"], :subject => params[:id], :job_id => review_params[:job_id]).present?
+          return unnecessary_error('review') if Review.all.where(:user_id => @decoded_token["sub"], :subject => params[:id], :job_id => review_params[:job_id]).present?
 
           #make review
           review = Review.new(review_params)
           review.subject = params[:id]
-          review.created_by = @decoded_token["sub"]
+          review.user_id = @decoded_token["sub"]
+          review.created_by = @decoded_token["sub"] # todo migrate to drop column
           review.save!
           render status: 200, json: { "message": "Review submitted!" }
         rescue ActionController::ParameterMissing # full review_params strong parameter missing
-          blank_error('review')
+          return blank_error('review')
         end
       end
 
@@ -151,7 +150,7 @@ module Api
       def review_params
         params.require(:review).permit(:rating, :message, :job_id)
       end
-=end
+
     end
   end
 end
