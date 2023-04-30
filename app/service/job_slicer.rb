42711a91c245b5d53bedbe3244f40189cba6a838
github4touchdouble <touchedouble@gmx.de>
2023-04-30 19:40:03 +0200

    deactivated job_slicer

--- app/service/job_slicer.rb
+++ app/service/job_slicer.rb
@@ -7,13 +7,14 @@ class JobSlicer
     if rad.nil?
       rad = 25000
     end
+=begin
     unless lat.nil? && lon.nil? #no database entry will be made (because there is no user.save!)
       user.latitude = lat
       user.longitude = lon
     end
     return SpatialJobValue.geo_query(user.latitude, user.longitude, rad, 500) #todo: add functionality that dynamically adapts rad according to the density of jobs in the area (to better consider differences between very densely populated urban areas and rural areas)
-    #return Job.all.limit(100)
+=end
+  return Job.all.limit(100)
   end
-  private
 
 end
