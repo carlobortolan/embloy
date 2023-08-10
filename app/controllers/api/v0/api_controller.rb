033dba59c9dd1f407498fc07a2df0143dc329d2a
Carlo Bortolan <106114526+carlobortolan@users.noreply.github.com>
2023-08-10 14:53:21 +0200

    Disabled prototype-auth for api requests

--- app/controllers/api/v0/api_controller.rb
+++ app/controllers/api/v0/api_controller.rb
@@ -5,6 +5,7 @@ module Api
     class ApiController < ApplicationController
       include ApiExceptionHandler
       protect_from_forgery with: :null_session
+      skip_before_action :auth_prototype
 
       def verify_access_token
         (request.headers["HTTP_ACCESS_TOKEN"].nil? || request.headers["HTTP_ACCESS_TOKEN"].empty?) ? blank_error('token') : @decoded_token = AuthenticationTokenService::Access::Decoder.call(request.headers["HTTP_ACCESS_TOKEN"])[0]
