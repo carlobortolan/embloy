904fc07b2b3966906858cd9885174009f0caf1f5
github4touchdouble <touchedouble@gmx.de>
2023-08-11 14:53:57 +0200

    updated JobUpdate model

--- app/models/job_update.rb
+++ app/models/job_update.rb
@@ -1,4 +1,18 @@
 class JobUpdate
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  field :job_id, type: Integer
+  field :user_id, type: Integer
+  field :trigger_type, type: String
+  field :update_message, type: String
+  field :successor, type: JobUpdate
+  field :timestamp, type: DateTime
+
+  enum trigger_type: {
+    clock_triggered: 1,
+    externally_triggered: 2
+  }
+
+  after_initialize :setup_change_stream
 end
