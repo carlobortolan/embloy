a7449801242d7b14a59e2729da55b98544ec60b6
github4touchdouble <touchedouble@gmx.de>
2023-08-12 12:00:43 +0200

    tidied up

--- app/models/job_assignment.rb
+++ app/models/job_assignment.rb
@@ -1,6 +1,7 @@
 class JobAssignment < ApplicationRecord
   belongs_to :job, counter_cache: true
   belongs_to :user, counter_cache: true, :dependent => :destroy
+
   validates :user_id, presence: true
   validates :job_id, presence: true
 end
