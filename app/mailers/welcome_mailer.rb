5c0a86261066a4bc2751f50bb09a106e2d986cee
carlobortolan <carlobortolan@gmail.com>
2023-08-27 22:35:17 +0200

    Updated welcome-token expiration

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,11 +2,8 @@ class WelcomeMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url = sign_in_url
-    @token = params[:user].signed_id(purpose: 'verify_account', expires_in: 15.minutes)
+    @token = params[:user].signed_id(purpose: 'verify_account')
     mail from: ENV['EMAIL_NOREPLY_USER'], to: @user.email, subject: 'Embloy - Welcome to Embloy'
-
-    # mail from: ENV['EMAIL_NOREPLY_USER'], to: @user.email, subject: 'Welcome to Embloy'
-    # mail(to: @user.email, subject: 'Welcome to What\'s Next')
   end
 
   def notify_team
