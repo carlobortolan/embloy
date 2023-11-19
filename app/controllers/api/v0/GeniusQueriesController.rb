03fac666a836b4aff6db48f30b3a78c377064f3a
github4touchdouble <touchedouble@gmx.de>
2023-11-19 19:04:16 +0100

    added basic controller method structure

--- app/controllers/api/v0/GeniusQueriesController.rb
+++ app/controllers/api/v0/GeniusQueriesController.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
-
 module Api
   module V0
     class GeniusQueriesController < ApiController
+      before_action :verify_access_token
       def create
-
+        begin
+          verified!(@decoded_token["typ"])
+          # token = GeniusQueryService::Encoder.call(@decoded_token["sub"], create_params)
+          # render status: 200, json: { "query_token" => token }
+        end
       end
 
       def query
+        begin
+          verified!(@decoded_token["typ"])
+          token = params[:token]
+          # res = GeniusQueryService::Decoder.call(@decoded_token["sub"], token)
+          # render status: 200, json: { "query_result" => res } #TODO: Enlarge status handling
+        end
+      end
 
+      private
+
+      def create_params
+        params.permit(:job_id, :user_id)
       end
 
     end
