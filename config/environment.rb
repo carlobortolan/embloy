5f1f106de2d2781d65097d916277b34b929cf448
carlobortolan <carlobortolan@gmail.com>
2024-01-06 15:53:44 +0100

    Update from env_var to .env

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@
 require_relative 'application'
 
 # Load environment variables
-env_var = File.join(Rails.root, 'config', 'env_var.rb')
-load(env_var) if File.exist?(env_var)
+# env_var = File.join(Rails.root, 'config', 'env_var.rb')
+# load(env_var) if File.exist?(env_var)
 
 # Initialize the Rails application.
 Rails.application.initialize!
