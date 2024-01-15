334118824b0c5159175b9d34cd29579ee583988e
carlobortolan <carlobortolan@gmail.com>
2024-01-15 01:31:38 +0100

    Add default cors urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,11 +2,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('CORS_CORE_CLIENT_URL', nil)
+    origins ENV.fetch('CORS_CORE_CLIENT_URL', 'http://localhost:3000')
     resource '*', headers: :any, methods: %i[get post put patch delete options head]
   end
   allow do
-    origins ENV.fetch('CORS_GENIUS_CLIENT_URL', nil)
+    origins ENV.fetch('CORS_GENIUS_CLIENT_URL', 'http://localhost:3000')
     resource '*', headers: :any, methods: %i[get post]
   end
 end
