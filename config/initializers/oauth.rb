84349bd799e5b4fd4e474e4aa85bf76f6d44c20b
carlobortolan <carlobortolan@gmail.com>
2024-02-15 04:27:30 +0100

    Add azure oauth

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -11,9 +11,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV.fetch('GOOGLE_OAUTH2_KEY', nil), ENV.fetch('GOOGLE_OAUTH2_SECRET', nil)
 end
 
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :azure_activedirectory_v2, ENV.fetch('AZURE_CLIENT_ID', nil), ENV.fetch('AZURE_CLIENT_SECRET', nil)
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :azure_activedirectory_v2, client_id: ENV.fetch('AZURE_CLIENT_ID', nil), client_secret: ENV.fetch('AZURE_CLIENT_SECRET', nil)
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, client_id: ENV.fetch('LINKEDIN_APP_ID', nil), client_secret: ENV.fetch('LINKEDIN_APP_SECRET', nil)
