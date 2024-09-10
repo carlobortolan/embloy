30efa3286226fb55077dc81900868b9378437554
carlobortolan <carlobortolan@gmail.com>
2024-09-10 19:51:45 +0200

    Add CI/CD config for AKS and ACA, update db config

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,6 @@
 # Default Rails ApplicationRecord class
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  # TODO: Enable this once we have a replica database
+  # connects_to database: { writing: :primary, reading: :primary_replica }
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -9,3 +9,15 @@ Rails.configuration.stripe = {
 Rails.configuration.after_initialize do
   Stripe.api_key = ENV.fetch('STRIPE_SECRET_KEY', nil)
 end
+
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    publishable_key: ENV.fetch('STRIPE_LIVE_PUBLISHABLE_KEY', nil),
+    secret_key: ENV.fetch('STRIPELIVE__SECRET_KEY', nil),
+    webhook_secret: ENV.fetch('STRIPE_LIVE_SIGNING_SECRET', nil)
+  }
+
+  Rails.configuration.after_initialize do
+    Stripe.api_key = ENV.fetch('STRIPE_LIVE_SECRET_KEY', nil)
+  end
+end
