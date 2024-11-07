fd814ff08f29bc8655ed16de3ed721e380591c84
carlobortolan <carlobortolan@gmail.com>
2024-11-07 02:05:08 +0100

    Add otp routes
    Signed-off-by: carlobortolan <carlobortolan@gmail.com>

--- spec/routing/api/v0/authentications_routing_spec.rb
+++ spec/routing/api/v0/authentications_routing_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe 'AuthenticationsController', type: :routing do
     it 'routes to #create_access' do
       expect(post: '/api/v0/auth/token/access').to route_to('api/v0/authentications#create_access', format: 'json')
     end
+
+    it 'routes to #create_otp' do
+      expect(post: '/api/v0/auth/token/otp').to route_to('api/v0/authentications#create_otp', format: 'json')
+    end
+
+    it 'routes to #verify_otp' do
+      expect(patch: '/api/v0/auth/token/otp').to route_to('api/v0/authentications#verify_otp', format: 'json')
+    end
   end
 end
