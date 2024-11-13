ffd814f8f85a148b9435a10861082b55a99dcb3e
carlobortolan <carlobortolan@gmail.com>
2024-11-13 15:04:24 +0100

    Add RSpec tests for job lists and job list items routing

---
+++ spec/requests/api/v0/job_list_items_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V0::JobListItemsController, type: :routing do
+  describe 'routing' do
+    it 'routes to #create' do
+      expect(post: '/api/v0/job_lists/1/items').to route_to('api/v0/job_list_items#create', job_list_id: '1')
+    end
+
+    it 'routes to #destroy' do
+      expect(delete: '/api/v0/job_lists/1/items/1').to route_to('api/v0/job_list_items#destroy', job_list_id: '1', id: '1')
+    end
+  end
+end
