d39fce211d967ffe7525d81b5ade19eed2d0f613
Winston Smith <touchedouble@gmx.de>
2024-12-13 17:49:59 +0100

    added genius service

---
+++ genius/target/debug/deps/genius-1c63991413e572e5.d
@@ -0,0 +1,5 @@
+D:\Home\hummel0\Software\Spontane Jobs\code\Embloy-Core-Server\genius\target\debug\deps\libgenius-1c63991413e572e5.rmeta: src\main.rs
+
+D:\Home\hummel0\Software\Spontane Jobs\code\Embloy-Core-Server\genius\target\debug\deps\genius-1c63991413e572e5.d: src\main.rs
+
+src\main.rs:
---
+++ genius/target/debug/deps/genius-b5b0ac39daeda8f6.d
@@ -0,0 +1,5 @@
+D:\Home\hummel0\Software\Spontane Jobs\code\Embloy-Core-Server\genius\target\debug\deps\libgenius-b5b0ac39daeda8f6.rmeta: src\main.rs
+
+D:\Home\hummel0\Software\Spontane Jobs\code\Embloy-Core-Server\genius\target\debug\deps\genius-b5b0ac39daeda8f6.d: src\main.rs
+
+src\main.rs:
